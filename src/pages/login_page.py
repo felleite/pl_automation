337b08e4a3946e8871a41f02568d62743053559e
Felipe Leite <felipedzn@gmail.com>
2025-04-01 12:43:03 -0300

    create main method

--- src/pages/login_page.py
+++ src/pages/login_page.py
@@ -1,4 +1,8 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
 
 class LoginPage:
     def __init__(self, driver):
@@ -11,3 +15,14 @@ class LoginPage:
         self.driver.find_element(*self.username_field).send_keys(username)
         self.driver.find_element(*self.password_field).send_keys(password)
         self.driver.find_element(*self.login_button).click()
+
+    def wait_until_visible(self, locator):
+        try:
+            element = WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located((By.ID, locator))
+            )
+            print("Element found")
+
+        except TimeoutException:
+            print("Error: The element was not found in the limit time")
+        return element
